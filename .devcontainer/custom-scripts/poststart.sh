952b11de3f59294d0781e90269fb91ed4ea5e985
Gabriel Lima <limagbz@outlook.com>
2023-06-01 18:50:16 +0000

    feat(devcontainer): config locale, setup postcreate commands

--- .devcontainer/custom-scripts/poststart.sh
+++ .devcontainer/custom-scripts/poststart.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
-
 YELLOW='\033[1;33m'
 BLUE='\033[1;34m'
 NC='\033[0m' # No Color
 
-echo -e "\n${YELLOW}Running Post Create Commands...${NC}"
+echo -e "\n${YELLOW}Running Post Start Commands...${NC}"
 
 echo -e "\n${BLUE}Upgrading Packages...${NC}"
 apt update
