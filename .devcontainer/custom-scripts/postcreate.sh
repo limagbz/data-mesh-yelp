7913210c9255525ca2d04b204dd1988a09113b4e
Gabriel Lima <limagbz@outlook.com>
2023-10-05 14:25:30 +0000

    feat(devcontainer): removing postcreate.sh

--- .devcontainer/custom-scripts/postcreate.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-YELLOW='\033[1;33m'
-BLUE='\033[1;34m'
-NC='\033[0m' # No Color
-
-echo -e "\n${YELLOW}Running Post Create Commands...${NC}"
-
-echo -e "\n${BLUE}Installing Custom Theme ZSHELL...${NC}"
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
